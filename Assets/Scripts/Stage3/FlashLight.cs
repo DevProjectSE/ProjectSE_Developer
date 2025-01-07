0796172086cf2075c913cd5945c5cab3ac540e26
DESKTOP-UG1U3PM\KGA <jaehongan56@gmail.com>
2025-01-07 17:34:41 +0900

    stage2 기능구현 - 특정 구간 지날 시 밝기 서서히 어두워지기 - 특정 구간 지날 시 이전으로 못가게 투명한 벽 설정
    stage3 작업공간 생성
    - 손전등 구현 중

---
+++ Assets/Scripts/Stage3/FlashLight.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class FlashLight : MonoBehaviour
+{
+    public GameObject flashLight;
+    private InputActionReference activateAction; 
+
+    private void Start()
+    {
+        flashLight.SetActive(false);
+    }
+
+
+    private void OnEnable()
+    {
+        if (activateAction != null) ;
+        {
+            activateAction.action.performed += GetOnLight;
+
+        }
+    }
+
+    private void OnDisable()
+    {
+        // 이벤트 연결 해제
+        if (activateAction != null)
+        {
+            activateAction.action.performed -= GetOnLight;
+        }
+    }
+    private void GetOnLight(InputAction.CallbackContext context)
+    {
+        if(flashLight !=null)
+        {
+            flashLight.SetActive(true);
+        }
+    }
+}
