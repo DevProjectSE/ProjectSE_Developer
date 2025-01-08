0d6920e29e7dbfd5e2e98c50d3af86d54d5366c8
DESKTOP-UG1U3PM\KGA <jaehongan56@gmail.com>
2025-01-08 15:11:07 +0900

    Stage3 UV손전등 구현 중

--- Assets/Scripts/Stage3/FlashLight.cs
+++ Assets/Scripts/Stage3/FlashLight.cs
@@ -8,35 +8,66 @@ using UnityEngine.XR.Interaction.Toolkit.Samples.StarterAssets;
 public class FlashLight : MonoBehaviour
 {
     public GameObject flashLight;
-    [SerializeField] private InputActionReference activateAction; 
+    [SerializeField] private InputActionReference leftActivateAction; 
+    [SerializeField] private InputActionReference rightActivateAction;
+
+    private int lightState = 0;
+
+    private Light lightComponent;
+    public Color basicColor = Color.white;
+    public Color UVColor = Color.blue;
 
     private void Start()
     {
+        lightComponent = flashLight.GetComponent<Light>();
         flashLight.SetActive(false);
     }
 
 
     private void OnEnable()
     {
-        if (activateAction != null)
+        if (leftActivateAction != null)
         {
-            activateAction.action.performed += GetOnLight;
+            leftActivateAction.action.performed += GetOnLight;
+
+        }
+        
+        if (rightActivateAction != null)
+        {
+            rightActivateAction.action.performed += GetOnLight;
 
         }
     }
 
     private void OnDisable()
     {
-        if (activateAction != null)
+        if (leftActivateAction != null)
         {
-            activateAction.action.performed -= GetOnLight;
+            leftActivateAction.action.performed -= GetOnLight;
+        }
+        if (rightActivateAction != null)
+        {
+            rightActivateAction.action.performed -= GetOnLight;
         }
     }
     private void GetOnLight(InputAction.CallbackContext context)
     {
-        if(flashLight !=null)
+        if(lightState==0)
         {
+
             flashLight.SetActive(true);
+            lightComponent.color = basicColor;
+            lightState = 1;
+        }
+        else if(lightState==1)
+        {
+            lightComponent.color = UVColor;
+            lightState = 2;
+        }
+        else if(lightState==2)
+        {
+            flashLight.SetActive(false);
+            lightState = 0;
         }
     }
 }
