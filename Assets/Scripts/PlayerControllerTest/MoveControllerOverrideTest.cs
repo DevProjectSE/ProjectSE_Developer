6096c46a6c80a110b3f783029cd04116901bdfd2
DESKTOP-99CV4K8\qpalz <qpalzm60515@gmail.com>
2025-01-09 02:43:47 +0900

    현재 컨트롤러 커스텀중, 오른쪽 컨트롤러 앉기 기능 구현(Default InputAction에 Turn South를 North까지 포함시킴), 회전 구현, 텔레포트 삭제, 프레임뷰어 추가, 플레이어 충돌체 설정

--- Assets/Scripts/PlayerControllerTest/MoveControllerOverrideTest.cs
+++ Assets/Scripts/PlayerControllerTest/MoveControllerOverrideTest.cs
@@ -3,38 +3,52 @@ using System.Collections.Generic;
 using Unity.XR.CoreUtils;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.XR.Interaction.Toolkit.Inputs;
 using UnityEngine.XR.Interaction.Toolkit.Inputs.Readers;
-using UnityEngine.XR.Interaction.Toolkit.Locomotion;
-
+using UnityEngine.XR.Interaction.Toolkit.Samples.StarterAssets;
 public class MoveControllerOverrideTest : MonoBehaviour
 {
     [SerializeField]
-    private InputActionReference activeAction;
-    private CharacterController cc;
+    private InputActionReference activeSitAction;
 
-    private void Awake()
-    {
-        cc = GetComponent<CharacterController>();
+    public Transform cam_Offset;
+    public Transform cam_StartPos;
+    public Transform cam_SitPos;
+    private bool isSit = false;
+
+    private void Start()
+    {   //카메라 offset을 제어해서 적용해보려고 했으나
+        //카메라의 위치에 따라 CharacterController의 크기가 제어됨.
+        // cam_Offset.position = cam_StartPos.position;
     }
-
     private void OnEnable()
     {
-        activeAction.action.performed += SitDown;
+        activeSitAction.action.performed += SitDown;
+        activeSitAction.action.performed += StandUp;
     }
 
     private void OnDisable()
     {
-        activeAction.action.performed -= SitDown;
+        activeSitAction.action.performed -= SitDown;
+        activeSitAction.action.performed -= StandUp;
     }
 
     private void SitDown(InputAction.CallbackContext context)
     {
         float valuey = context.ReadValue<Vector2>().y;
-        if (valuey < 0)
+        if (valuey < 0 && isSit == false)
         {
-            Debug.Log("앉으라 ");
-            cc.height = 0.45f;
+            cam_Offset.position = cam_SitPos.position;
+            isSit = true;
+        }
+    }
+
+    private void StandUp(InputAction.CallbackContext context)
+    {
+        float valuey = context.ReadValue<Vector2>().y;
+        if (valuey > 0 && isSit)
+        {
+            cam_Offset.position = cam_StartPos.position;
+            isSit = false;
         }
     }
 }
