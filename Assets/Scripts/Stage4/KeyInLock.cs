00abf1cfde7ba09a54e754e2712d52d3e18705d0
KimChanYoung <qpalzm605@gmail.com>
2025-01-08 12:46:21 +0900

    자물쇠 프리팹화 완료.

--- Assets/Scripts/Stage4/KeyInLock.cs
+++ Assets/Scripts/Stage4/KeyInLock.cs
@@ -8,18 +8,29 @@ public class KeyInLock : MonoBehaviour
 
     private XRKnob knob;
     public Transform openHint;
-    public GameObject upperPart;
+    public Transform upper_Part_LOD0;
+    public List<BoxCollider> upper_Part_collsList = new();
+
+    private bool isActived = false;
+
     private void Awake()
     {
         knob = GetComponent<XRKnob>();
     }
 
     public void OnActive()
-    {
-        if (knob.value >= 0.01f)
+    {   //이미 풀렸으면 이후에는 OnActive 호출 안하도록 처리
+        if (isActived) return;
+        //열쇠를 끝까지 돌렸는지 확인
+        if (knob.value <= 0.01f)
         {
-            upperPart.transform.position = openHint.position;
+            upper_Part_LOD0.transform.position = openHint.position;
+            foreach (BoxCollider coll in upper_Part_collsList)
+            {
+                coll.enabled = false;
+                isActived = true;
+            }
+            Debug.Log("삘리리릴");
         }
-        Debug.Log($"knob val : {knob.value}");
     }
 }
