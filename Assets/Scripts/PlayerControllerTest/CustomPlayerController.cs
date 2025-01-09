5364582a571de73f2843dca8a179305d89612f8e
KimChanYoung <qpalzm605@gmail.com>
2025-01-09 21:23:39 +0900

    기존 리지드바디로 플레이어 충돌체 제어를 캐릭터 컨트롤러로 다시 바꿨습니다.(기획팀과 협의하에 버그 발생 요인이 많을것 같아서 변경했습니다.) 현재 카메라 위치가 중심을 벗어나면 컨트롤러 제어 불가하게 설정하려고 하는중입니다.

--- Assets/Scripts/PlayerControllerTest/CustomPlayerController.cs
+++ Assets/Scripts/PlayerControllerTest/CustomPlayerController.cs
@@ -5,38 +5,44 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.XR.Interaction.Toolkit.Inputs.Readers;
 using UnityEngine.XR.Interaction.Toolkit.Samples.StarterAssets;
-public class MoveControllerOverrideTest : MonoBehaviour
+public class CustomPlayerController : MonoBehaviour
 {
     [SerializeField]
     private InputActionReference activeSitAction;
-
+    [SerializeField]
+    private InputActionReference activePrimaryX;
+    private CharacterController CC;
     public Transform cam_Offset;
     public Transform cam_StartPos;
     public Transform cam_SitPos;
     private bool isSit = false;
-    public Camera camera;
-    XROrigin origin;
-    private void Awake()
-    {
-        origin = GetComponent<XROrigin>();
-    }
 
     private void Start()
     {   //카메라 offset을 제어해서 적용해보려고 했으나
         //카메라의 위치에 따라 CharacterController의 크기가 제어됨.
         // cam_Offset.position = cam_StartPos.position;
-
+        CC = GetComponent<CharacterController>();
     }
     private void OnEnable()
     {
         activeSitAction.action.performed += SitDown;
         activeSitAction.action.performed += StandUp;
+        // activePrimaryX.action.performed += Primary;
+
     }
 
     private void OnDisable()
     {
         activeSitAction.action.performed -= SitDown;
         activeSitAction.action.performed -= StandUp;
+        // activePrimaryX.action.performed -= Primary;
+
+    }
+
+    private void Primary(InputAction.CallbackContext context)
+    {
+        float value = context.ReadValue<float>();
+        Debug.Log("하이");
     }
 
     private void SitDown(InputAction.CallbackContext context)
@@ -45,6 +51,7 @@ public class MoveControllerOverrideTest : MonoBehaviour
         if (valuey < 0 && isSit == false)
         {
             cam_Offset.position = cam_SitPos.position;
+            CC.height = cam_SitPos.position.y;
             isSit = true;
         }
     }
@@ -55,6 +62,7 @@ public class MoveControllerOverrideTest : MonoBehaviour
         if (valuey > 0 && isSit)
         {
             cam_Offset.position = cam_StartPos.position;
+            CC.height = cam_StartPos.position.y;
             isSit = false;
         }
     }
